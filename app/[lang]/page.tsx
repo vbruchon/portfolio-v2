16bf5dd98c233590f98f3937a45646d2eec64957
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2025-08-22 16:46:23 +0200

    feat: setup internalization

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,8 +1,18 @@
-export default function Home() {
+import { getDictionary } from "@/locales/dictionnaries";
+
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ lang: string }>;
+}) {
+  const { lang } = await params;
+  const dict = await getDictionary(lang, "hero");
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         Portfolio v2
+        <p>{dict.hello}</p>
       </main>
     </div>
   );
