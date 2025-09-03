81abce94a64a245ee125a56408a128ec402aab4a
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2025-09-03 18:05:37 +0200

    chore(hero): change me img and adjust css

--- src/components/section/hero/hero-img.tsx
+++ src/components/section/hero/hero-img.tsx
@@ -7,12 +7,12 @@ export const HeroImage: FC<{
   children?: React.ReactNode;
 }> = ({ src, alt, children }) => (
   <div className="relative w-80 aspect-square md:w-96 lg:w-[400px] xl:w-[450px] 2xl:w-[500px] flex justify-center items-start mr-8 md:mr-0">
-    <div className="absolute inset-0 rounded-full overflow-hidden z-10">
+    <div className="absolute inset-0 rounded-b-[50%] overflow-y-hidden z-10">
       <Image
         src={src}
         alt={alt}
         fill
-        className="object-cover -translate-x-[10%]"
+        className="object-cover -translate-x-[10%] -rotate-2 translate-y-[5%]"
         priority
       />
     </div>
